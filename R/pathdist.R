3980a31196d7a9457b95ef599aeafe8cf7dfb9f9
Sarah Goslee <sarah.goslee@usda.gov>
2023-09-14 20:36:05 -0400

    cleanup

--- R/pathdist.R
+++ R/pathdist.R
@@ -1,8 +1,9 @@
-mstdist <- function(v, maxv = 1) {
+pathdist <- function(v, maxv = 1) {
 
 	# v is a lower-triangular distance matrix
 	# maxv is the cutoff for distances: values greater or equal to this will be estimated 
 	# from the shortest path along the distance-weighted graph connecting the samples
+    # note that this will not work with completely disconnected subsets
 
 	n <- (1 + sqrt(1 + 8 * length(v)))/2
 	if (abs(n - round(n)) > 1e-07)
@@ -17,7 +18,7 @@ mstdist <- function(v, maxv = 1) {
     v.ind$d <- v
     v.ind <- subset(v.ind, d < maxv)
 
-    v.graph2 <- add_edges(make_empty_graph(n), edges = t(v.ind[, 1:2]), weight = v.ind$d)
+    v.graph <- add_edges(make_empty_graph(n, directed = FALSE), edges = t(v.ind[, 1:2]), weight = v.ind$d)
 
 	v.dist <- distances(v.graph)
 	v.dist <- lower(v.dist)
